76e48a1c42370ea0dd37db5ca091a09ec6e66c00
wilfred nas <wilfred@wnas.nl>
2017-05-23 13:53:48 +0200

    first commit

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,7 +1,9 @@
-﻿import http = require('http');
-var port = process.env.port || 1337
+"use strict";
+var http = require("http");
+var port = process.env.port || 1337;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
     // comment just to commit
 }).listen(port);
+//# sourceMappingURL=server.js.map
